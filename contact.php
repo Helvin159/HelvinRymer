fe26238830318a734860d7edf0c9e07764f0e39e
Helvin Rymer <helvinrymer@Helvins-Air.home>
2020-07-02 12:26:28 -0400

    email spam filter update

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@ $source = $_GET['source'];
 $comment = $_GET['comment'];
 
 
-if ($email == null || $name == null){
+if ($email == null){
 
 	header('location:index.html');
 } else if($email == "tetakhiling2015@mail.ru" || $email == "riyaadkins.sc.1045423808@supersendme.org" || $email == "noreply@papawp6211.xyz" || $email == "annamaster1992211@gmail.com" || $email == "aliciashort.gm.1326626298@gcheck.xyz" || $email == "noreply@get-more-leads-now.com" || $email == "no-reply@hilkom-digital.de" || $email == "helvinrymernewyork.com@domstat.su" || $email == "maiilerinvestorsx@gmx.com" || $email == "helvinrymer.com@domstat.su" || $email == "helvinrymer.com@domstat.su"){ 
@@ -26,9 +26,9 @@ mysqli_close($connection);
 
 $to = "Helvin@HelvinRymer.com"; // this is your Email address
 $from = $email; // this is the sender's Email address
-$subject = "Form submission";
+$subject = "New Message from $email";
 $subject2 = "Thank you for your email!";
-$message = "$name sent the following details:
+$message = "$email sent the following details:
 Email: $email
 Website: $url
 Source: $source
