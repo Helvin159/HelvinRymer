1b5d8b2b4fbcc5b15ff3df0e73959dbf8484b825
Helvin159 <helvinrymer@Helvins-MBP.home>
2020-10-13 17:50:53 -0400

    updates

--- partials/section1.php
+++ partials/section1.php
@@ -7,7 +7,7 @@
 				<h1 class="display-1 text-left">01</h1>
 			</div>
 			<div class="row title text-center" id="title1">
-				<h1 class=" "><span><mark>Academy</mark></span></h1>
+				<h1 class=" m-auto p-5"><span><mark>Academy</mark></span></h1>
 			</div>
 			</div>
 		</div>
